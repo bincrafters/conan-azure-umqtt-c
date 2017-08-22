6f05952efa8e30265c23e5bcff7522a024add585
Uilian Ries <uilianries@gmail.com>
2017-08-22 19:26:15 -0300

    Update Azure-C-Shared-Utility dependency
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
-from conans import ConanFile, CMake, os, tools
-import shutil
+from os import path
+from conans import ConanFile, CMake, tools
 
 
 class AzureUMQTTCConan(ConanFile):
@@ -11,11 +11,11 @@ class AzureUMQTTCConan(ConanFile):
     source_url = "https://github.com/Azure/azure-umqtt-c"
     description = "General purpose library for communication over the mqtt protocol"
     license = "https://github.com/Azure/azure-iot-sdk-c/blob/master/LICENSE"
-    lib_short_name = "azure-umqtt-c"
+    requires = "Azure-C-Shared-Utility/1.0.41@bincrafters/stable"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
     release_date = "2017-08-11"
-    requires = "Azure-C-Shared-Utility/%s@bincrafters/testing" % version
     release_name = "%s-%s" % (name.lower(), release_date)
-    exports = ["azure_c_shared_utilityConfig.cmake"]
 
     def source(self):
         tools.get("https://github.com/Azure/azure-umqtt-c/archive/%s.tar.gz" % self.release_date)
@@ -27,18 +27,20 @@ class AzureUMQTTCConan(ConanFile):
         '''
         tools.replace_in_file("%s/CMakeLists.txt" % self.release_name, "project(umqtt)", conan_magic_lines)
         cmake = CMake(self)
-        cmake.definitions["use_installed_dependencies"] = True
         cmake.definitions["skip_samples"] = True
+        cmake.definitions["use_installed_dependencies"] = True
         cmake.definitions["azure_c_shared_utility_DIR"] = self.deps_cpp_info["Azure-C-Shared-Utility"].res_paths[0]
         cmake.configure(source_dir=self.release_name)
         cmake.build()
 
     def package(self):
-        include_dir = os.path.join(
-            self.lib_short_name, "inc", self.lib_short_name.replace("-","_"))
-
-        self.copy(pattern="*", dst="include", src="include_dir")
-        self.copy(pattern="*.lib", dst="lib", src="")
+        self.copy(pattern="LICENSE", dst=".", src=".")
+        self.copy(pattern="*", dst="include", src=path.join(self.release_name, "inc"))
+        self.copy(pattern="*.lib", dst="lib", src="lib")
+        self.copy(pattern="*.dll", dst="bin", src=".")
+        self.copy(pattern="*.a", dst="lib", src="lib")
+        self.copy(pattern="*.so*", dst="lib", src=".")
+        self.copy(pattern="*.dylib", dst="bin", src=".")
 
     def package_info(self):
         self.cpp_info.libs = self.collect_libs()
