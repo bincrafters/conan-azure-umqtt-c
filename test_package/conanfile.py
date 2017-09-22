fc566471aa1ccb747172f4ef0ac2dd587aa8d58b
Jerry W <jerrywiltse@gmail.com>
2017-09-22 00:12:42 -0400

    standardize test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,26 +1,24 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
-import platform
 
 
-class AzurecsharedutilityTestConan(ConanFile):
+class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    user = os.getenv("CONAN_USERNAME", "bincrafters")
-    channel = os.getenv("CONAN_CHANNEL", "testing")
-    
+
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
-
+        
+    def imports(self):
+        self.copy("*", dst="bin", src="lib")
+        
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
-
+        self.copy("*.so*", dst="bin", src="lib")        
+        self.copy("*.cmake", dst="res", src="res")
+        
     def test(self):
-        os.chdir("bin")
-        if platform.system() == "Windows":
-            self.run("iot_c_utility.exe")
-        else:
-            self.run("./iot_c_utility")
+        self.run(os.path.join("bin","test_package"))
